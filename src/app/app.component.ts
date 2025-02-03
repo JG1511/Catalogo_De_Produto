7a34139cb31faf77761efb5472e3e59c67aea57c
JG1511 <joaotsi2024@gmail.com>
2025-02-03 10:10:59 -0300

    add butao de excluir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,65 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Produto } from './produto';
-import { Catalogo } from './catalogo';
-import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
-import { InputNumberModule } from 'primeng/inputnumber';
+import { RouterOutlet } from '@angular/router';
+import { MessageService, ConfirmationService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
+import { Catalogo } from './catalogo';
+import { Produto } from './produto';
+import { ToastModule } from 'primeng/toast';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,InputTextModule, FormsModule,InputNumberModule, ButtonModule,TableModule],
+  imports: [RouterOutlet, InputTextModule, FormsModule, InputNumberModule, ButtonModule, TableModule, ConfirmDialogModule,ToastModule],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  id : number = 0;
-  nome : string = '';
-  preco : number = 0;
-  categoria : string = '';
+  id: number = 0;
+  nome: string = '';
+  preco: number = 0;
+  categoria: string = '';
 
-  percentualDesconto : number = 0;
+  percentualDesconto: number = 0;
 
-  produtoSelecionado ? : Produto;
-  catalogo : Catalogo = new Catalogo();
+  produtoSelecionado?: Produto;
+  catalogo: Catalogo = new Catalogo();
+
+  constructor(
+    private confirmationService: ConfirmationService,
+    private messageService : MessageService
+  ) {
+  }
 
   onIncluir(): void {
-    let produto = new Produto(this.id,this.nome,this.preco,this.categoria);
+    let produto = new Produto(this.id, this.nome, this.preco, this.categoria);
     this.catalogo.adicionarProduto(produto);
     this.id = 0;
     this.nome = '';
     this.preco = 0;
     this.categoria = '';
+  }
+
+  deleteProduct(produto: Produto) {
+    this.confirmationService.confirm({
+      message: 'Você tem certeza em deletar o ' + produto.nome + '?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.catalogo.removerProduto(produto.id)
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Successful',
+          detail: 'Product Deleted',
+          life: 3000
+        });
+      }
+    });
   }
 }
